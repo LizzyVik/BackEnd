66935ce1db98c3d92c8359318688114b1c68b7f6
pollet <pollete.alarcon@gmail.com>
2019-07-26 01:01:57 -0400

    Api Producto

--- Backend_Inventario/src/main/java/com/backendInventario/ModelEntity/Productos.java
+++ Backend_Inventario/src/main/java/com/backendInventario/ModelEntity/Productos.java
@@ -25,64 +25,80 @@ private Marcas marca;
 private String color;
 @OneToOne
 private  Categorias categoría;
-@OneToOne
-private Precios valorProducto;
+@Column
+private Long precioVenta;
 @OneToOne
 private Stock stock;
+
 
 public Integer getId() {
 	return id;
 }
+
+
 public void setId(Integer id) {
 	this.id = id;
 }
+
+
 public String getModelo() {
 	return modelo;
 }
+
+
 public void setModelo(String modelo) {
 	this.modelo = modelo;
 }
-public Precios getValorProducto() {
-	return valorProducto;
-}
-public void setValorProducto(Precios valorProducto) {
-	this.valorProducto = valorProducto;
-}
+
+
 public Marcas getMarca() {
 	return marca;
 }
+
+
 public void setMarca(Marcas marca) {
 	this.marca = marca;
 }
+
+
 public String getColor() {
 	return color;
 }
+
+
 public void setColor(String color) {
 	this.color = color;
 }
+
+
 public Categorias getCategoría() {
 	return categoría;
 }
+
+
 public void setCategoría(Categorias categoría) {
 	this.categoría = categoría;
 }
+
+
+public Long getPrecioVenta() {
+	return precioVenta;
+}
+
+
+public void setPrecioVenta(Long precioVenta) {
+	this.precioVenta = precioVenta;
+}
+
+
 public Stock getStock() {
 	return stock;
 }
+
+
 public void setStock(Stock stock) {
 	this.stock = stock;
 }
-@Override
-public String toString() {
-	return "Producto [id=" + id + ", modelo=" + modelo + ", marca=" + marca + ", color=" + color + ", categoría="
-			+ categoría + ", valorProducto=" + valorProducto + ", stock=" + stock + "]";
-}
 
-
-
-
-
-	
-	
 
 }
