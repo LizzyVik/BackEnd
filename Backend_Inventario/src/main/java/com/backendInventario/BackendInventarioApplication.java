231e887082e233f195b053ad651f6687cb6f58ba
Rene Allende <rene.allende15@gmail.com>
2019-08-02 22:03:15 -0400

    comentario de autores en el main class

--- Backend_Inventario/src/main/java/com/backendInventario/BackendInventarioApplication.java
+++ Backend_Inventario/src/main/java/com/backendInventario/BackendInventarioApplication.java
@@ -1,5 +1,6 @@
 package com.backendInventario;
 
+//Autores Rene Allende, Eliana Gutierres,  Joselyn Pino
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
