0a68f2508ef7c88017058a6b0761b43143bf51b7
Rene Allende <rene.allende15@gmail.com>
2019-08-05 01:06:09 -0400

    Se Agrega precio venta

--- Backend_Inventario/src/main/java/com/backendInventario/model/Producto.java
+++ Backend_Inventario/src/main/java/com/backendInventario/model/Producto.java
@@ -18,70 +18,56 @@ public class Producto {
 	private String color;
 	@OneToOne
 	private Categoria categoria;
-	private Integer precio;
+	private Integer precioVenta;
 	private Integer cantidadMax;
 	private Integer cantidadMin;
-
+	
+	
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getModelo() {
 		return modelo;
 	}
-
 	public void setModelo(String modelo) {
 		this.modelo = modelo;
 	}
-
 	public Marca getMarca() {
 		return marca;
 	}
-
 	public void setMarca(Marca marca) {
 		this.marca = marca;
 	}
-
 	public String getColor() {
 		return color;
 	}
-
 	public void setColor(String color) {
 		this.color = color;
 	}
-
 	public Categoria getCategoria() {
 		return categoria;
 	}
-
 	public void setCategoria(Categoria categoria) {
 		this.categoria = categoria;
 	}
-
-	public Integer getPrecio() {
-		return precio;
+	public Integer getPrecioVenta() {
+		return precioVenta;
 	}
-
-	public void setPrecio(Integer precio) {
-		this.precio = precio;
+	public void setPrecioVenta(Integer precioVenta) {
+		this.precioVenta = precioVenta;
 	}
-
 	public Integer getCantidadMax() {
 		return cantidadMax;
 	}
-
 	public void setCantidadMax(Integer cantidadMax) {
 		this.cantidadMax = cantidadMax;
 	}
-
 	public Integer getCantidadMin() {
 		return cantidadMin;
 	}
-
 	public void setCantidadMin(Integer cantidadMin) {
 		this.cantidadMin = cantidadMin;
 	}
